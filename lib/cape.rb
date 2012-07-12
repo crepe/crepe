5bab2ad6e4d47b6401dfe4d66786d0f6aa9bd951
Evan Owen <kainosnoema@gmail.com>
2012-07-11 19:44:15 -0700

    Add '#scoped' to index scopes, 'error' => 'exception'
    Just some minor bug fixes and consistancy improvements.
    
    Signed-off-by: Stephen Celis <stephen@recurly.com>
    Signed-off-by: Evan Owen <kainosnoema@gmail.com>

--- lib/cape.rb
+++ lib/cape.rb
@@ -1,7 +1,9 @@
 module Cape
+
   autoload :API,        'cape/api'
   autoload :Endpoint,   'cape/endpoint'
   autoload :Middleware, 'cape/middleware'
   autoload :Params,     'cape/params'
   autoload :VERSION,    'cape/version'
+
 end
--- lib/cape/version.rb
+++ lib/cape/version.rb
@@ -1,3 +1,5 @@
 module Cape
+
   VERSION = '0.0.1.pre'
+
 end
