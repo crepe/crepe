ae26db842051912fcf32538932b42d9bef60e1a6
Stephen Celis <stephen@stephencelis.com>
2012-08-01 15:25:53 -0700

    Fix acceptance filter spec
    The filter moved but the spec didn't.
    
    Signed-off-by: Stephen Celis <stephen@stephencelis.com>

--- spec/lib/crepe/endpoint/filter/acceptance_spec.rb
+++ spec/lib/crepe/endpoint/filter/acceptance_spec.rb
@@ -1,8 +1,8 @@
 require 'ostruct'
-require_relative '../../../../lib/crepe/endpoint/acceptance'
+require_relative '../../../../../lib/crepe/endpoint/filter/acceptance'
 
-describe Crepe::Endpoint::Acceptance do
-  subject { Crepe::Endpoint::Acceptance }
+describe Crepe::Endpoint::Filter::Acceptance do
+  subject { Crepe::Endpoint::Filter::Acceptance }
   let(:endpoint) {
     # FIXME: Test an actual endpoint?
     OpenStruct.new config: { formats: %w[json] }, format: :pdf
