d065a9ba21572181eb1fffccab0095e769344267
Stephen Celis <stephen@stephencelis.com>
2012-08-06 16:10:41 -0700

    Document Cape::Util::ChainedInclude
    Signed-off-by: Stephen Celis <stephen@stephencelis.com>

--- lib/cape/util/chained_include.rb
+++ lib/cape/util/chained_include.rb
@@ -1,23 +1,57 @@
 module Cape
   module Util
+    # Forwards all future includes to objects that have already been
+    # extended by (or have included) the parent module.
+    #
+    # Default Ruby behavior:
+    #
+    #   # A module is defined...
+    #   module A
+    #   end
+    #
+    #   # ...and included in a class.
+    #   class B
+    #     include Module A
+    #   end
+    #
+    #   # Given another module...
+    #   module C
+    #     def c
+    #     end
+    #   end
+    #
+    #   # ...included in the first...
+    #   module A
+    #     include Module C
+    #   end
+    #
+    #   # ...the class will not have access to its methods.
+    #   B.new.c # NoMethodError: undefined method `c' for #<B>
+    #
+    # To prevent the above error, the original module could have
+    # included ChainedInclude:
+    #
+    #   module A
+    #     extend ChainedInclude
+    #   end
     module ChainedInclude
 
       private
 
         def include mod
           super
-          extended_by.each { |child| child.extend mod }
-          included_by.each { |child| child.__send__ :include, mod }
+          extended_by.each { |object| object.extend mod }
+          included_by.each { |base| base.__send__ :include, mod }
         end
 
-        def extended child
+        def extended object
           super
-          extended_by << child
+          extended_by << object
         end
 
-        def included child
+        def included base
           super
-          included_by << child
+          included_by << base
         end
 
         def extended_by
