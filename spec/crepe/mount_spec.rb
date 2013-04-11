5b03e13fb1b18c81a12ce15bd71383cad277310b
Evan Owen <kainosnoema@gmail.com>
2013-04-10 19:56:35 -0700

    Remove support for extending mounted helpers
    Signed-off-by: Evan Owen <kainosnoema@gmail.com>

--- spec/crepe/mount_spec.rb
+++ spec/crepe/mount_spec.rb
@@ -7,19 +7,6 @@ describe Crepe::API, '.mount' do
     namespace :pong do
       mount -> env { [200, {}, ['KO']] }
     end
-
-    api = Class.new Crepe::API do
-      get do
-        greeting
-      end
-    end
-    mount api
-
-    helper do
-      def greeting
-        'Hello, world!'
-      end
-    end
   end
 
   it 'mounts Rack apps at paths' do
@@ -28,9 +15,5 @@ describe Crepe::API, '.mount' do
 
   it 'mounts Rack apps within namespaces' do
     get('/pong').body.should eq 'KO'
-  end
-
-  it 'mounts Crepe::APIs and extends their endpoints' do
-    get('/').body.should include 'Hello, world!'
   end
 end
