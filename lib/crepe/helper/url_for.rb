dde729c6620d7f6c3b439c4505e5756822378c7c
Evan Owen <kainosnoema@gmail.com>
2013-04-16 19:25:32 -0700

    Temporarily fix URLFor helper
    Content negotiation is not currently used for storing version
    information.
    
    Signed-off-by: Stephen Celis <stephen.celis@gmail.com>
    Signed-off-by: Evan Owen <kainosnoema@gmail.com>

--- lib/crepe/helper/url_for.rb
+++ lib/crepe/helper/url_for.rb
@@ -2,18 +2,13 @@ module Crepe
   module Helper
     module URLFor
 
+      #--
+      # FIXME: doesn't work with path/query versioning
+      #++
       def url_for *args, **options
         url = "#{request.scheme}://#{request.host_with_port}"
-        if version
-          options[:v] = version if env['crepe.content_negotiation'] == :query
-          url << "/#{version}" if env['crepe.content_negotiation'].nil?
-        end
         args.each { |c| url << "/#{c.to_param}" }
-        extension = options.delete :format do
-          if File.extname(env['crepe.original_path_info']) == ".#{format}"
-            format
-          end
-        end
+        extension = options.delete(:format) { format }
         url << ".#{extension}" if extension
         url << "?#{options.to_query}" unless options.empty?
         url
